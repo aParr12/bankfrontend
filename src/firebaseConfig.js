bf695d5e933fd01c1999913d9489c2b501d7af42
aParr12 <adamparring@gmail.com>
2022-12-14 02:08:55 -0500

    Fix: env typo

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -5,7 +5,7 @@ export const firebaseConfig = {
   apiKey: process.env.API_KEY,
   authDomain: process.env.AUTH_DOMAIN,
   projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.SOTRAGE_BUCKET,
+  storageBucket: process.env.STORAGE_BUCKET,
   messagingSenderId: process.env.MESSAGEING_SENDER_ID,
   appId: process.env.APP_ID,
   measurementId: process.env.MEASUREMENT_ID,
