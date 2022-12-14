57c0610c93fea8953ac984c507425f6c59db2056
aParr12 <adamparring@gmail.com>
2022-12-14 03:10:40 -0500

    Update: add base url to env

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
-import React from 'react';
+import React from "react";
 import { BrowserRouter } from "react-router-dom";
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.css';
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "bootstrap/dist/css/bootstrap.css";
 
-export const SERVER_PATH = 'http://localhost:3001';
+export const SERVER_PATH = process?.env?.REACT_APP_BASE_URL ?? "http://localhost:3001";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <React.StrictMode>
-      <BrowserRouter>
-        <App/>
-      </BrowserRouter>
-    </React.StrictMode>
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
