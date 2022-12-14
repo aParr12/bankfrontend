2957584e2d40307faefaaafcafed0b9616fe52a0
aParr12 <adamparring@gmail.com>
2022-12-14 02:02:20 -0500

    Update: env

---
+++ src/firebaseConfig.js
@@ -0,0 +1,12 @@
+// TODO: Replace the following with your app's Firebase project configuration
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfig = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.SOTRAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGEING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID,
+};
